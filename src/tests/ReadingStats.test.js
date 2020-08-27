0a79b2abd0157c7c0c7fddc93bddfca22f979c0d
Maurice Tadros <maurice.tadros@gmail.com>
2020-08-27 10:09:07 -0600

    Added more tests

--- src/tests/ReadingStats.test.js
+++ src/tests/ReadingStats.test.js
@@ -2,10 +2,9 @@
  * Internal dependencies
  */
 import readingStats from "../ReadingStats";
-
 import html23 from "./data/23";
 import html42 from "./data/42";
-// import html60 from "./data/60";
+import html60 from "./data/60";
 
 describe("My Test Suite", () => {
   it("Test Blog Frontity 23", () => {
@@ -16,33 +15,38 @@ describe("My Test Suite", () => {
 
   it("Test Blog Frontity 42", () => {
     const stats = readingStats(html42.rendered);
-    console.log(stats);
-
-    // var img_sources = jsdom.env(html42.rendered, function (errors, window) {
-    //   var imgs = window.document.getElementsByTagName("img");
-    //   for (var i = 0; i < imgs.length; i++) {
-    //     var src = imgs[i].getAttribute("src");
-    //     if (src) console.log(src);
-    //   }
-    // });
-
     expect(stats.words).toEqual(260);
     expect(stats.images).toEqual(2);
   });
 
-  it("Test Blog Frontity 42", () => {
+  it("Test Blog Frontity Null", () => {
     const stats = readingStats(null);
-    console.log(stats);
-
-    // var img_sources = jsdom.env(html42.rendered, function (errors, window) {
-    //   var imgs = window.document.getElementsByTagName("img");
-    //   for (var i = 0; i < imgs.length; i++) {
-    //     var src = imgs[i].getAttribute("src");
-    //     if (src) console.log(src);
-    //   }
-    // });
-
     expect(stats.words).toEqual(0);
     expect(stats.images).toEqual(0);
+  });
+
+  it("Test Blog Frontity 60", () => {
+    const stats = readingStats(html60.rendered);
+    expect(stats.words).toEqual(352);
+    expect(stats.images).toEqual(1);
+  });
+
+  it("Test Images speed", () => {
+    const stats = readingStats(html60.rendered, 200, 1);
+    expect(stats.words).toEqual(352);
+    expect(stats.images).toEqual(1);
+
+    //352/200 = 1.76 + 1 (image) = 2.76
+    expect(stats.minutes).toEqual(2.76);
+    console.log(stats);
+  });
+
+  it("Test Reading speed", () => {
+    const stats = readingStats(html60.rendered, 352, 1);
+    expect(stats.words).toEqual(352);
+    expect(stats.images).toEqual(1);
+
+    //352/352 = 1 + 1 (image) = 2
+    expect(stats.minutes).toEqual(2);
   });
 });
