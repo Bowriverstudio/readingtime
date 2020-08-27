d467c0383fb3b93c83f689cdd1688ef0bd642dfe
Maurice Tadros <maurice.tadros@gmail.com>
2020-08-27 09:56:03 -0600

    Added Snapshot

--- src/tests/ReadingTime.test.js
+++ src/tests/ReadingTime.test.js
@@ -12,5 +12,6 @@ import ReadingTime from "../ReadingTime";
 it("ReadingTime renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<ReadingTime />, div);
+  expect(div).toMatchSnapshot();
   ReactDOM.unmountComponentAtNode(div);
 });
